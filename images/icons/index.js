f12e53719320c5dfc420758d812805fdb16e8132
Mohammad Ali Amidi <Amidi@aramico.com>
2023-09-03 17:09:39 +0330

    add icons

---
+++ images/icons/index.js
@@ -0,0 +1,19 @@
+import phone from './icons8_phone.svg';
+import email from './icons8_mail.svg';
+import search from './icons8_search.svg';
+import twitter from './icons8_twitter.svg';
+import instagram from './icons8_instagram.svg';
+import linkedin from './icons8_linkedin.svg';
+import youtube from './icons8_youtube1.svg';
+import language from './icons8_globe_1.svg';
+
+export {
+    phone,
+    email,
+    search,
+    twitter,
+    instagram,
+    linkedin,
+    youtube,
+    language
+};
