83c231701a02cf50a77953e82bbf287463afa4d8
Mohammad Ali Amidi <Amidi@aramico.com>
2023-09-23 17:09:35 +0330

    Slider

---
+++ images/sliders/index.js
@@ -0,0 +1,11 @@
+import Slider_1 from './Slider-1.jpg';
+import Slider_2 from './Slider-2.jpg';
+import Slider_3 from './Slider-3.jpeg';
+import Slider_4 from './Slider-4.jpg';
+
+export {
+    Slider_1,
+    Slider_2,
+    Slider_3,
+    Slider_4,
+};
