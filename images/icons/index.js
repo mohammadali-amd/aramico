6ba06c0dfa45db40a1c5d52b709adda2ed368e06
Mohammad Ali Amidi <Amidi@aramico.com>
2023-10-14 16:29:15 +0330

    Add CTA Buttons

--- images/icons/index.js
+++ images/icons/index.js
@@ -6,6 +6,11 @@ import instagram from './icons8_instagram.svg';
 import linkedin from './icons8_linkedin.svg';
 import youtube from './icons8_youtube1.svg';
 import language from './icons8_globe_1.svg';
+import cta_phone from './phone.svg';
+import cta_chat_message from './chat_message_sent.svg';
+import cta_email_open from './email_open.svg';
+import cta_messaging from './messaging.svg';
+import cta_youtube from './youtube.svg';
 
 export {
     phone,
@@ -15,5 +20,10 @@ export {
     instagram,
     linkedin,
     youtube,
-    language
+    language,
+    cta_phone,
+    cta_chat_message,
+    cta_email_open,
+    cta_messaging,
+    cta_youtube,
 };
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import HomePage from '@/sections/HomePage'
-
-const inter = Inter({ subsets: ['latin'] })
+import CTA from '@/components/CTA'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      {/* <div className="flex min-h-screen flex-col items-center justify-between mt-80">
-        <HomePage />
-      </div> */}
       <HomePage />
+      <CTA />
       <Footer />
     </main>
-  )
+  );
 }
